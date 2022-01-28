da66da4d5082a60fd5e2f18eb5ba9a04380238af
0802uni <87895769+0802uni@users.noreply.github.com>
2022-01-28 18:29:53 +0900

    InputFieldでエラー
    二人目以降の名前を返さない

---
+++ Assets/Scripts/PlayerGenerater.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerGenerater : MonoBehaviour
+{
+    int playerNum;
+
+    [SerializeField]
+    GameObject playerNameInput;
+
+    GameObject parentObject;
+
+    GameObject playerPrefab;
+
+    List<string> playerNames;
+
+    [SerializeField]
+    GameObject playerNumInput;
+
+    [SerializeField]
+    List<InputField> playerNameInputs;
+
+    [SerializeField]
+    Button startButton;
+
+    public void DecideNum()
+    {
+        var num = playerNumInput.GetComponentInChildren<InputField>().text;
+        parentObject = playerNumInput.transform.parent.gameObject;
+
+        playerNum = int.Parse(num);
+
+        playerNumInput.SetActive(false);
+
+        for (int i = 0; i < playerNum; i++)
+        {
+            var input=Instantiate(playerNameInput, parentObject.transform);
+            input.GetComponentInChildren<Text>().text = "player" + (i+1) + ":";
+            playerNameInputs.Add(input.GetComponentInChildren<InputField>());
+        }
+
+        startButton.interactable = true;
+    }
+
+    public void DecideName()
+    {
+        foreach (var n in playerNameInputs)
+        {
+            playerNames.Add(n.text);
+        }
+
+        DontDestroyOnLoad(this);
+        SceneManager.LoadScene("MainScene");
+
+        GameObject playerBoard = GameObject.Find("PlayerBoard");
+
+        foreach (var n in playerNames)
+        {
+            var p = Instantiate(playerPrefab);
+            p.GetComponent<Player>().myName = n;
+        }
+    }
+}
